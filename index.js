f1cfbccc5c8e461b29b12d4a0866ee09de2216ba
Karan Rathod <karanrathod@Karans-MacBook-Air-2.local>
2021-05-23 23:14:47 +0530

    add CRUD functionality and logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const app = express();
 
 let contacts = [
@@ -19,8 +20,17 @@ let contacts = [
   },
 ];
 
+// middleware
 app.use(express.json());
+app.use(logger("tiny"));
+logger.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  logger(
+    ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
+  )
+);
 
+// routes
 app.get("/api/persons", (req, res) => {
   res.json(contacts);
 });
@@ -30,6 +40,44 @@ app.get("/info", (req, res) => {
     contacts.length - 1
   } people ${new Date()}`;
   res.json(response);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  let id = +req.params.id;
+  let response = contacts.find((contact) => contact.id === id);
+  if (response) {
+    res.json(response);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  let id = +req.params.id;
+  contacts = contacts.filter((contact) => contact.id !== id);
+  return res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  // error handling
+  let checkIfContactExists = contacts.find(
+    (contact) => contact.name === req.body.name
+  );
+
+  if (checkIfContactExists) {
+    res.status(400).send({ error: "name must be unique" });
+  } else if (!req.body.name) {
+    res.status(400).send({ error: "name is missing" });
+  } else if (!req.body.number) {
+    res.status(400).send({ error: "number is missing" });
+  }
+
+  // adding a new entry
+  let id = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
+  let contact = req.body;
+  contact.id = id;
+  contacts = contacts.concat(contact);
+  return res.json(contact);
 });
 
 const port = 3002;
