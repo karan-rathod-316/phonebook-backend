9b283c0e4e84ea7d5a84d273849b17b2899cc1ee
Karan Rathod <karanrathod@Karans-MacBook-Air-2.local>
2021-05-31 12:57:42 +0530

    add mongo playground to test some functionalities with the db

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://phonebook:${password}@phonebook-cluster.eclug.mongodb.net/contacts?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+const contact = new Contact({
+  name: `${process.argv[3]}`,
+  number: +`${process.argv[4]}`,
+});
+
+// contact.save().then((result) => {
+//   mongoose.connection.close();
+// });
+
+if (!process.argv[4]) {
+  Contact.find({}).then((result) =>
+    result.forEach((res) => {
+      console.log(res);
+      mongoose.connection.close();
+    })
+  );
+}
